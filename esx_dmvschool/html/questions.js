7c8957cd92f38f79cd456ed8086bc7eb9b8864c9
Cybernetisch <64712616+Cybernetisch@users.noreply.github.com>
2020-09-08 18:15:05 +0200

    Add files via upload

---
+++ esx_dmvschool/html/questions.js
@@ -0,0 +1,91 @@
+var tableauQuestion = [
+	{
+		question: "Als u 80 km/h rijdt en een woonplaats nadert, betekent dit dat u:",
+		propositionA: "Gaat versnellen",
+		propositionB: "Uw snelheid behouden als, u geen andere voertuigen passeert",
+		propositionC: "U remt af",
+		propositionD: "U kunt uw snelheid behouden",
+		reponse: "C"
+	},
+
+	{
+		question: "U staat op het punt bij het groene licht rechtsaf te slaan, maar u ziet een voetganger over steken:",
+		propositionA: "Je gaat voor de voetganger",
+		propositionB: "U controleert of er geen ander voertuig is en u passeert",
+		propositionC: "Je wacht tot de voetganger klaar is",
+		propositionD: "Je roept te voetganger om te passeren",
+		reponse: "C"
+	},
+
+	{
+		question: "Zonder enige indicatie: De snelheid in een woonwijk is:__km/h",
+		propositionA: "30",
+		propositionB: "45",
+		propositionC: "55",
+		propositionD: "60",
+		reponse: "A"
+	},
+
+	{
+		question: "Voor elke verandering van rijstrook moet u:",
+		propositionA: "In uw spiegels kijken",
+		propositionB: "Uw blinde vlekken controleren",
+		propositionC: "Uw bedoelingen aangeven",
+		propositionD: "Alles van dit",
+		reponse: "D"
+	},
+
+	{
+		question: "Welk alcoholpromillage is geclassificeerd als rijden onder invloed?",
+		propositionA: "0.05%",
+		propositionB: "0.18%",
+		propositionC: "0.08%",
+		propositionD: "0.06%",
+		reponse: "B"
+	},
+
+	{
+		question: "Wanneer mag u doorrijden bij verkeerslichten?",
+		propositionA: "Als het groen is",
+		propositionB: "Als er niemand op de kruising is",
+		propositionC: "Als u zich in een schoolzone bevindt",
+		propositionD: "Als het groen en/of rood is en ik sla rechts af.",
+		reponse: "A"
+	},
+
+	{
+		question: "Een voetganger staat bij een rood stoplicht voor voetgangers",
+		propositionA: "Je laat hem/haar passeren",
+		propositionB: "Je observeert voordat je verder gaat",
+		propositionC: "Je zwaait naar hem/haar",
+		propositionD: "Je gaat rijd verder want je licht is groen",
+		reponse: "A"
+	},
+
+	{
+		question: "Wat moet u doen als u een ander voertuig passeert?",
+		propositionA: "Volg hem op de voet om hem sneller in te halen",
+		propositionB: "Passeer hem door de weg te verlaten",
+		propositionC: "Rijd op de linkerbaan om in te halen",
+		propositionD: "Overschrijd het snelheidslimiet",
+		reponse: "C"
+	},
+
+	{
+		question: "U rijdt op een snelweg die een maximale snelheid aangeeft van 100 km/h. Het meeste verkeer is 110 km/h, dus u moet niet harder rijden dan? :",
+		propositionA: "80 kmh",
+		propositionB: "40 kmh",
+		propositionC: "50 kmh",
+		propositionD: "100 kmh",
+		reponse: "D"
+	},
+
+	{
+		question: "Waneer u door een ander voertuig wordt gepasseerd, is het belangrijk om dit NIET te doen :",
+		propositionA: "Vertragen",
+		propositionB: "Kijk in je spiegels",
+		propositionC: "Bekijk andere chauffeurs",
+		propositionD: "Verhoog uw snelheid",
+		reponse: "D"
+	},
+]
